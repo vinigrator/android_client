19cf6728b93a3cd550ffdff8520897a0368c36db
Anton Arjakov <zdj22@yandex.ru>
2016-03-29 01:31:50 +0300

    full choose photo

--- pry/app/src/main/java/com/example/pry/Main.java
+++ pry/app/src/main/java/com/example/pry/Main.java
@@ -1,7 +1,9 @@
 package com.example.pry;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +14,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main extends AppCompatActivity {
 
@@ -32,7 +42,7 @@ public class Main extends AppCompatActivity {
         btnCamera.setOnClickListener(new btnCameraClick());
 
         // код для бинда галереи к кнопке галлереи
-        Button button = (Button)findViewById(R.id.btnGallery);
+        Button button = (Button) findViewById(R.id.btnGallery);
         button.setOnClickListener(new btnGalleryClick());
     }
 
@@ -42,52 +52,51 @@ public class Main extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(data==null)
+        if (data == null)
             return;
 
+        Intent intent = new Intent(this, faceDetect.class);
+
         // если ответ от камеры
-        if(requestCode == CAM_REQUEST)
-        {
-            if(data.hasExtra("data")) {
+        if (requestCode == CAM_REQUEST) {
+            if (data.hasExtra("data")) {
                 bmp = (Bitmap) data.getExtras().get("data");
                 // есть изображением в bmp
-
+                intent.putExtra("bmp", bmp);
+                intent.putExtra("stts", "bmp");
             }
 
-        }else if(requestCode == GALLERY_REQUEST)
-        {
-            if(resultCode == RESULT_OK){
-                Uri selectedImage = data.getData();
-                try {
-                    bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // есть изображение в bmp
+        }
+        Uri u = null;
+        if (resultCode == RESULT_OK) {
+            if (requestCode == GALLERY_REQUEST) {
+                u = (Uri) data.getData();
+                intent.putExtra("uri", u);
+                intent.putExtra("stts", "uri");
             }
         }
 
-        Intent intent = new Intent(Main.this, faceDetect.class);
-        intent.putExtra("bmp", bmp);
         startActivity(intent);
+
     }
 
     // событие для кнопки снимка
-    class btnCameraClick implements Button.OnClickListener
-    {
+    class btnCameraClick implements Button.OnClickListener {
         @Override
-        public void onClick(View v)
-        {
+        public void onClick(View v) {
             Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(camIntent, CAM_REQUEST);
         }
     }
 
-    class btnGalleryClick implements  Button.OnClickListener{
+    class btnGalleryClick implements Button.OnClickListener {
         @Override
         public void onClick(View v) {
             Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
             photoPickerIntent.setType("image/*");
+            //photoPickerIntent.setAction(Intent.ACTION_GET_CONTENT);
+            //photoPickerIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            //startActivityForResult(intent, SELECT_PICTURE);
             startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
         }
     }
--- pry/app/src/main/java/com/example/pry/faceDetect.java
+++ pry/app/src/main/java/com/example/pry/faceDetect.java
@@ -1,10 +1,19 @@
 package com.example.pry;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.camera2.params.Face;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class faceDetect extends AppCompatActivity {
 
@@ -16,7 +25,17 @@ public class faceDetect extends AppCompatActivity {
         ImageView iv = (ImageView) findViewById(R.id.imgPhoto);
 
         Intent intent = getIntent();
-        Bitmap bmp = (Bitmap)intent.getExtras().getParcelable("bmp");
-        iv.setImageBitmap(bmp);
+        String status = intent.getStringExtra("stts");
+
+        if(status.contentEquals("bmp")) {
+            Bitmap bmp = (Bitmap) intent.getExtras().getParcelable("bmp");
+            iv.setImageBitmap(bmp);
+        }
+        if(status.contentEquals("uri"))
+        {
+            Uri u = (Uri)intent.getExtras().getParcelable("uri");
+            iv.setImageURI(u);
+        }
     }
+
 }
