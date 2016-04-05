adb4232dee389cd91965a15d052ce53eb6eeca1b
Anton Arjakov <zdj22@yandex.ru>
2016-04-05 19:15:37 +0300

    find photo complete

--- pry/app/src/main/java/com/example/pry/Main.java
+++ pry/app/src/main/java/com/example/pry/Main.java
@@ -5,9 +5,11 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,10 +21,13 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends AppCompatActivity {
 
@@ -31,6 +36,7 @@ public class Main extends AppCompatActivity {
     Bitmap bmp;         // объект взятого изображения
     private static final int CAM_REQUEST = 1313;    // код реквеста камеры
     private static final int GALLERY_REQUEST = 1;   // код реквеста галлереи
+    private static Uri mOutputFileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,22 +58,27 @@ public class Main extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (data == null)
-            return;
-
         Intent intent = new Intent(this, faceDetect.class);
 
+        Uri u = null;
         // если ответ от камеры
         if (requestCode == CAM_REQUEST) {
-            if (data.hasExtra("data")) {
-                bmp = (Bitmap) data.getExtras().get("data");
-                // есть изображением в bmp
-                intent.putExtra("bmp", bmp);
-                intent.putExtra("stts", "bmp");
+            if (data != null) {
+                if (data.hasExtra("data")) {
+                    bmp = (Bitmap) data.getExtras().get("data");
+                    // есть изображением в bmp
+                    intent.putExtra("bmp", bmp);
+                    intent.putExtra("stts", "bmp");
+                }
+            } else {
+                //u = (Uri)data.getExtras().get(MediaStore.EXTRA_OUTPUT);
+                //data.getData();
+                intent.putExtra("uri", mOutputFileUri);
+                intent.putExtra("stts", "uri");
             }
 
         }
-        Uri u = null;
+
         if (resultCode == RESULT_OK) {
             if (requestCode == GALLERY_REQUEST) {
                 u = (Uri) data.getData();
@@ -85,6 +96,20 @@ public class Main extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Time time = new Time();
+            time.setToNow();
+            File checkDir = new File(Environment.getExternalStorageDirectory() + "/pry/Images");
+
+            if(checkDir.exists()) {
+                Log.v(LOG_TAG, "directory exist");
+            } else {
+                Log.v(LOG_TAG, "directory NOT exist");
+                checkDir.mkdirs();
+                Log.v(LOG_TAG, "directory create");
+            }
+            File file = new File(Environment.getExternalStorageDirectory()+"/pry/Images", Integer.toString(time.year) + Integer.toString(time.month) + Integer.toString(time.monthDay) + Integer.toString(time.hour) + Integer.toString(time.minute) + Integer.toString(time.second) + ".jpg");
+            mOutputFileUri = Uri.fromFile(file);
+            camIntent.putExtra(MediaStore.EXTRA_OUTPUT, mOutputFileUri);
             startActivityForResult(camIntent, CAM_REQUEST);
         }
     }
